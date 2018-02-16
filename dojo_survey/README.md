29bd70b4fb453f0f4fbff61e7d645955d07cc241
Shannon Steen <Steen.ShannonM@gmail.com>
2018-02-16 12:59:45 -0500

    added form validation

--- dojo_survey/README.md
+++ dojo_survey/README.md
@@ -1,3 +1,5 @@
 Learning Flask - Week 1 Assignment 
-Assignment: Build a flask application that accepts a form submission, redirects and presents the submitted data on a results page.
-For this assigment, render from a post route.
+Assignment: Build a flask application that accepts a form submission, redirects and presents the submitted data on a results page. For this assignment, render from a post route.
+
+UPDATES
+Add validation to the form. Validations to include - name and comment fields are not blank, and the comment field is no longer than 120 characters.
--- dojo_survey/dojo_survey.py
+++ dojo_survey/dojo_survey.py
@@ -1,13 +1,27 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 app = Flask(__name__)
+app.secret_key = "Id0n0tLVpi"
 @app.route('/')
 def index():
     return render_template('index.html')
 @app.route('/results', methods=['POST']) #this route will handle our form submission; defined which HTTP methods are allowed by this route
 def get_results():
-    name = request.form['name']
-    location = request.form['dojo_location']
-    language = request.form['favorite_language']
-    comment = request.form['comment']
-    return render_template('dojo_survey_results.html', name=name, location=location, language=language, comment=comment)
+    # establish variables
+    session['name'] = request.form['name']
+    session['location'] = request.form['dojo_location']
+    session['language'] = request.form['favorite_language']
+    session['comment'] = request.form['comment']
+    # validate the input
+    if len(request.form['name']) < 1:  
+        flash("You must enter a name.")
+        return redirect('/')
+    if len(request.form['comment']) < 1: 
+        flash("You must enter a comment. Keep it short (no more than 120 characters).")
+        return redirect('/')
+    elif len(request.form['comment']) > 120: 
+        flash("Thanks for the feedback, but please limit comments to 120 characters or less.")
+        return redirect('/')
+    else:
+        # display the results
+        return render_template('dojo_survey_results.html', name=session['name'], location=session['location'], language=session['language'], comment=session['comment'])
 app.run(debug=True)
