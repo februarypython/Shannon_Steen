318fb99d0b05231c6cc0c5257d44587892ac4e04
Shannon Steen <Steen.ShannonM@gmail.com>
2018-03-30 23:01:01 -0400

    added private msg feature

--- django_assignments/user_dashboard/README.md
+++ django_assignments/user_dashboard/README.md
@@ -3,3 +3,6 @@ This is one of the best assignments you can do to prepare for the qualifying bel
 
 Build an user dashboard app in which a user can create, edit, view and message users.
 wireframe http://s3.amazonaws.com/General_V88/boomyeah/company_209/chapter_3834/handouts/chapter3834_6841_user-dashboard.pdf
+
+Additional Features Added:
+    ability to mark messages private; private messages (any any related comments) are only viewable by sender/receiver
--- django_assignments/user_dashboard/apps/communiques/models.py
+++ django_assignments/user_dashboard/apps/communiques/models.py
@@ -13,15 +13,25 @@ class CommuniqueManager(models.Manager):
         return errors
 
     def add_message(self, postData, to_user, from_user):
+        print postData
+        if 'private' in postData:  #user marked private message
+            priv = 0
+        else:
+            priv = 1
         msg_from = User.objects.get(id=from_user)
         msg_to = User.objects.get(id=to_user)
-        this_msg = self.create(message=postData['message'], msg_from=msg_from, msg_to=msg_to)
+        this_msg = self.create(message=postData['message'], privacy=priv, msg_from=msg_from, msg_to=msg_to)
         return this_msg 
 
 class Communique(models.Model):
+    STATUS_CHOICES = (
+        (0, 'private'),
+        (1, 'public'),
+    )
     message = models.TextField()
     msg_from = models.ForeignKey(User, related_name="written_msgs", on_delete=models.CASCADE)
     msg_to = models.ForeignKey(User, related_name="received_msgs", on_delete=models.CASCADE)
+    privacy = models.IntegerField(choices=STATUS_CHOICES, default=1)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     #connect instance of UserManager overwriting old objects key with new properties
